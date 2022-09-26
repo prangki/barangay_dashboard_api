bebf3f8064f41ab0ae3f0f020aed56e574726731
LAPTOP-7NCQ7GHR\JP <serifsan1@gmail.com>
2022-09-26 10:37:02 +0800

    UPDATE (Sept. 26, 2022) - Appointment Feature (Notice Doc)

--- webapi/Controllers/STLPartylistDashboardContorller/Features/AppointedMemberController.cs
+++ webapi/Controllers/STLPartylistDashboardContorller/Features/AppointedMemberController.cs
@@ -19,6 +19,7 @@ namespace webapi.Controllers.STLPartylistDashboardContorller.Features
         private readonly IAppointedMemberRepository _repo;
         private readonly IAccountRepository _loginrepo;
 
+
         public AppointedMemberController(IConfiguration config, IAppointedMemberRepository repo, IAccountRepository loginrepo)
         {
             _repo = repo;
@@ -45,6 +46,54 @@ namespace webapi.Controllers.STLPartylistDashboardContorller.Features
             var result = await _repo.LoadAppointMember();
             if (result.result == Results.Success)
                 return Ok(result.appoint);
+            else
+                return NotFound();
+
+        }
+
+        [HttpPost]
+        [Route("appoint/member/withdraw")]
+        public async Task<IActionResult> WithdrawAppointedMember(string appointid)
+        {
+            var result = await _repo.WithdrawAppointedMember(appointid);
+            if (result.result == Results.Success)
+                return Ok(new { result = result.result, message = result.message });
+            else if (result.result == Results.Failed)
+                return Ok(new { result = result.result, message = result.message });
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("appoint/member/remove")]
+        public async Task<IActionResult> RemoveAppointedMember(string appointid)
+        {
+            var result = await _repo.RemoveAppointedMember(appointid);
+            if (result.result == Results.Success)
+                return Ok(new { result = result.result, message = result.message });
+            else if (result.result == Results.Failed)
+                return Ok(new { result = result.result, message = result.message });
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("appoint/member/detail")]
+        public async Task<IActionResult> ViewAppointedMemberDetails(string id)
+        {
+            var result = await _repo.ViewAppointedMemberDetails(id);
+            if (result.result == Results.Success)
+                return Ok(result.details);
+            else
+                return NotFound();
+
+        }
+
+        [HttpPost]
+        [Route("appoint/document")]
+        public async Task<IActionResult> ViewDocumentTags(string id)
+        {
+            var result = await _repo.ViewDocumentTags(id);
+            if (result.result == Results.Success)
+                return Ok(result.details);
             else
                 return NotFound();
 
